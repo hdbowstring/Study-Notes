2bcac87a103b162c4f647fd698871494523450ad
연수현 <109198794+justsuhyun@users.noreply.github.com>
2022-10-10 15:38:06 +0900

    Add files via upload
    연습한 코드들 ~8-3

---
+++ 쉽게 배우는 자바1 (부스트코스)/java1/Data_and_Operation/StringApp.java
@@ -0,0 +1,16 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World"); // String
+		System.out.println('H'); // Character
+		System.out.println("H");
+		
+		System.out.println("Hello "
+				+ "World");
+		System.out.println("Hello \nWorld"); //new line
+		System.out.println("Hello \"World\""); // escape
+	}
+
+}
