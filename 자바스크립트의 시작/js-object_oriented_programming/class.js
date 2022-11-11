b8eed9fb904d9472d5698530d2f0d807d8d1fc8b
연수현 <109198794+hdbowstring@users.noreply.github.com>
2022-11-11 22:41:20 +0900

    ~12까지

--- 자바스크립트의 시작/js-object_oriented_programming/class.js
+++ 자바스크립트의 시작/js-object_oriented_programming/class.js
@@ -6,21 +6,38 @@ class Person{
         console.log('constructor');
     }
     sum(){
-        return 'prototype : '+(this.first+this.second);
+        return this.first+this.second;
     }
-
-}
+//     avg(){
+//         return (this.first+this.second)/2;
+//     }
+// }
+//함수를 추가하고 싶을 때, 클래스가 업데이트될 가능성도 있고, 거의 쓰이지는 않는 코드라면 코드를 수정하지 않고 상속을 사용
 
 // Person.prototype.sum = function(){
 //     return 'prototype : '+(this.first+this.second);
-// }
+}
+class PersonPlus extends Person{
+    constructor(name, first, second, third){
+        super(name, first, second); // super()는 부모클래스의 생성자
+        this.third = third;
+    }
+    sum(){
+        return super.sum()+this.third;
+    }
+    avg(){
+        return (this.first+this.second+this.third)/3;
+    }
+}
 
-var kim = new Person('kim', 10, 20);
-console.log('kim', kim);
-kim.sum = function(){
-    return 'this : '+(this.first+this.second);
-} 
-var lee = new Person('lee', 10, 10);
+var kim = new PersonPlus('kim', 10, 20, 30);
+// var kim = new Person('kim', 10, 20);
+// console.log('kim', kim);
+// kim.sum = function(){
+//     return 'this : '+(this.first+this.second);
+// } 
+// var lee = new Person('lee', 10, 10);
 console.log("kim.sum()", kim.sum()); 
-console.log("lee.sum()", lee.sum()); 
+console.log("kim.avg()", kim.avg()); 
+// console.log("lee.sum()", lee.sum()); 
   
